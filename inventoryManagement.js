1dc9dc204433bc86f08da8970f0c0a5ff1f7eeb6
Ken <kenmills89x@gmail.com>
2026-06-04 08:57:52 -0500

    Lab: Product Inventory Manager Complete

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,33 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(name) {
+  products.push(name);
+}
 
+function updateProductName(position, newName) {
+  if (position >= 0 && position < products.length) {
+    products[position] = newName;
+  } else {
+    console.log(`Error: Position ${position} is out of bounds`);
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 // Export the necessary parts for testing
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+  logFirstProduct:
+    typeof logFirstProduct !== "undefined" ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== "undefined" ? addProduct : undefined,
+  updateProductName:
+    typeof updateProductName !== "undefined" ? updateProductName : undefined,
+  removeLastProduct:
+    typeof removeLastProduct !== "undefined" ? removeLastProduct : undefined,
+  products,
 };
